36e93a18bf595a50d06e5770b1144390d5570d63
xtgxiso <124960772@qq.com>
2016-11-03 16:23:50 +0800

    Create redis_demo.php

---
+++ benchmark/php/redis_demo.php
@@ -0,0 +1,46 @@
+<?php
+use Workerman\Worker;
+use Workerman\Protocols\Http;
+use WebWorker\Libs\Mredis;
+
+//判断系统
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    require_once __DIR__.'/../workerman-for-win/Autoloader.php';
+}else {
+    require_once __DIR__.'/../workerman/Autoloader.php';
+    // 检查扩展
+    if(!extension_loaded('pcntl'))
+    {
+        exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
+    }
+
+    if(!extension_loaded('posix'))
+    {
+        exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
+    }
+}
+
+$app = new WebWorker\App("http://0.0.0.0:1215");
+
+//进程数
+$app->count = 4;
+
+//自动加载目录--会加载目录下的所有php文件
+$app->autoload = array();
+
+
+$config = array();
+$config["host"] = "127.0.0.1";
+$config["port"] = 6379;
+$config["password"] = "test";
+$config["db"] = 1;
+
+//注册路由
+$app->HandleFunc("/",function($conn,$data) use($app,$config){
+    $redis = Mredis::getInstance($config);
+    $conn->send($redis->get("xtgxiso"));
+});
+
+
+// Run worker
+Worker::runAll();
