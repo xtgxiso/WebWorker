b514349292ec840ece89e68e497bc31b922b1e61
xtgxiso <124960772@qq.com>
2016-11-03 11:00:19 +0800

    Update App.php

--- WebWorker/App.php
+++ WebWorker/App.php
@@ -56,7 +56,12 @@ class App extends Worker
 
     public function onClientMessage($connection,$data){
         if ( empty($this->map) ){
-            $connection->send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架<br/>没有添加路由，请添加路由!</div>');
+            $str = '<div style="margin: 200px auto;width:600px;height:800px;text-align:left;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架.没有添加路由，请添加路由!
+                    <pre>$app->HandleFunc("/",function($conn,$data) use($app){
+                            $conn->send("默认页");
+                    });</pre>
+                    </div>';
+            $connection->send($str);
             return;
         }
         $this->conn = $connection;
