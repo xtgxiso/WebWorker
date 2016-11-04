d9db58a5052ea935b36063c055146964fc6606a9
rpnanhai <rpnanhai@gmail.com>
2016-11-04 18:53:21 +0800

    优化警告错误提示

--- WebWorker/Libs/Mredis.php
+++ WebWorker/Libs/Mredis.php
@@ -15,14 +15,14 @@ class Mredis extends \Redis{
         $key = md5(implode(":",$config));
         if (!isset(self::$_instance[$key])) {
             self::$_instance[$key] = new self();
-            $host = $config["host"] ? $config["host"] : "127.0.0.1";
-            $port = $config["port"] ? $config["port"] : 6379;
+            $host = isset($config["host"]) ? $config["host"] : "127.0.0.1";
+            $port = isset($config["port"]) ? $config["port"] : 6379;
             self::$_instance[$key]->connect($host,$port);
-            $password = $config["password"] ? $config["password"] : "";
+            $password = isset($config["password"]) ? $config["password"] : "";
             if ( $password ){
                 self::$_instance[$key]->auth($password);
             }
-            $db = $config["db"] ? $config["db"] : 0;
+            $db = isset($config["db"]) ? $config["db"] : 0;
             if ( !self::$_instance[$key]->select($db) ){
                 echo "redis can't connect\r\n";
             }
