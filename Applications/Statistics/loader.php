ac716f30f1b799e6de75fa9ef001ad92b4fa3e76
xtgxiso <124960772@qq.com>
2016-11-07 10:56:14 +0800

    Update loader.php

--- Applications/Statistics/loader.php
+++ Applications/Statistics/loader.php
@@ -11,7 +11,21 @@
  * @link http://www.workerman.net/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-require_once __DIR__ . '/../../vendor/autoload.php';
+//判断系统
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    require_once __DIR__.'/../../workerman-for-win/Autoloader.php';
+}else {
+    require_once __DIR__.'/../../workerman/Autoloader.php';
+    // 检查扩展
+    if(!extension_loaded('pcntl'))
+    {
+        exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
+    }
+    if(!extension_loaded('posix'))
+    {
+        exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
+    }
+}
 require_once __DIR__ .'/Config/Config.php';
 require_once __DIR__.'/Protocols/Statistic.php';
 require_once __DIR__.'/Bootstrap/StatisticProvider.php';
