2bcc744389ba456a5cd7038836bda500eed2561f
xtgxiso <124960772@qq.com>
2016-11-08 16:11:12 +0800

    Update index.php

--- Applications/Demo1/controllers/index.php
+++ Applications/Demo1/controllers/index.php
@@ -1,4 +1,8 @@
 <?php
-$app->HandleFunc("/welcome",function($conn,$data) use($app){
-    $conn->send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架</div>');
+$app->HandleFunc("/welcome",function() use($app){
+    $app->server_send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架</div>');
+});
+
+app->HandleFunc("/",function() use($app){
+    $app->server_send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架</div>');
 });
