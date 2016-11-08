3b227c3721c25df4276744fc94eef4b1d0adccc4
xtgxiso <124960772@qq.com>
2016-11-08 16:17:16 +0800

    Update index.php

--- Applications/Demo1/controllers/index.php
+++ Applications/Demo1/controllers/index.php
@@ -3,6 +3,6 @@ $app->HandleFunc("/welcome",function() use($app){
     $app->server_send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架</div>');
 });
 
-app->HandleFunc("/",function() use($app){
+$app->HandleFunc("/",function() use($app){
     $app->server_send('<div style="margin: 200px auto;width:500px;height:800px;text-align:center;">基于<a href="http://www.workerman.net/" target="_blank">Workerman</a>实现的自带http server的web开发框架</div>');
 });
